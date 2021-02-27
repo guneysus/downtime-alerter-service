40e8ff616eba3e728d448020bcab3f8bc882d9c9
Ahmed Şeref Güneysu <serefguneysu@gmail.com>
2021-02-27 10:42:06 +0300

    DowntimeAlerterWeb project bootstrapped

---
+++ src/DowntimeAlerterWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DowntimeAlerterWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
